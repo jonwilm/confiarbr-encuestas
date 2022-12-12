6fd3d98eb4719245049de5b847295b0a212e4ca3
jonwilm <jparedes84@gmail.com>
2022-12-11 23:42:11 -0400

    settings prod

--- confiarbr/settings.py
+++ confiarbr/settings.py
@@ -1,7 +1,7 @@
 from .base_settings import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = False
 
 # Production
 # -------------------------------------
